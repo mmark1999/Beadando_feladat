a8692479ad041937cf64efeb39444832b3908f4d
Mark Majoros <majorosmark99@gmail.com>
2022-11-05 18:42:33 +0100

    Added the structure of the used SQL tables

---
+++ sql/beadando.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1:3306
+-- Létrehozás ideje: 2022. Nov 05. 17:41
+-- Kiszolgáló verziója: 5.7.36
+-- PHP verzió: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `beadando`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE latin2_hungarian_ci NOT NULL,
+  `manufacture` varchar(100) COLLATE latin2_hungarian_ci NOT NULL,
+  `type` varchar(100) COLLATE latin2_hungarian_ci NOT NULL,
+  `vintage` int(50) NOT NULL,
+  `milage` int(100) NOT NULL,
+  `fuel` varchar(100) COLLATE latin2_hungarian_ci NOT NULL,
+  `color` varchar(100) COLLATE latin2_hungarian_ci NOT NULL,
+  `price` int(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `user` varchar(300) COLLATE latin2_hungarian_ci NOT NULL,
+  `pass` varchar(128) COLLATE latin2_hungarian_ci NOT NULL,
+  `email` varchar(300) COLLATE latin2_hungarian_ci NOT NULL,
+  `is_admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
