b2f1e2250d7e82a0f4e0fcb78b6e9658a30d8c51
Mark Majoros <majorosmark99@gmail.com>
2022-11-13 14:46:29 +0100

    Added the state property of the products

--- index.php
+++ index.php
@@ -14,23 +14,38 @@
     <div class = "list">
     <table>
         <tr>
-            <th>Cím</th>
             <th>Gyártmány</th>
             <th>Típus</th>
             <th>Évjárat</th>
             <th>Futásteljesítmény</th>
             <th>Üzemanyag</th>
             <th>Szín</th>
+            <th>Állapot</th>
             <th>Ár</th>
         </tr>
         <form method="POST", action=".">
         <?php
+
+            function stars($state)
+            {
+                $stars = '';
+                for($i = 0; $i<$state; $i++)
+                {
+                    $stars = $stars.'<img class = "star" src="/img/star.png">';
+                }
+                for($i = 0; $i<5-$state; $i++)
+                {
+                    $stars = $stars.'<img class = "star" src="/img/empty_star.png">';
+                }
+                return $stars;
+            }
+
             $table = mysqli_query($conn,"SELECT * FROM `products`");
             while($row = mysqli_fetch_array($table))
             {
-                echo '<label id="'.$row['id'].'"><tr id = '.$row['id'].'><th>'.$row['title'].'</th><th>'.$row['manufacture'].
+                echo '<label id="'.$row['id'].'"><tr id = '.$row['id'].'><th>'.$row['manufacture'].
                 '</th><th>'.$row['type'].'</th><th>'.$row['vintage'].'</th><th>'.$row['milage'].
-                '</th><th>'.$row['fuel'].'</th><th>'.$row['color'].'</th><th>'.$row['price'].
+                '</th><th>'.$row['fuel'].'</th><th>'.$row['color'].'</th><th class = "star_container">'.stars($row['state']).'</th><th>'.$row['price'].
                 '</th></label><th><input type = "image" src="/img/trolley.png" class="trolley" 
                 style="';
                 if(isset($_SESSION["isauthenticated"]))
